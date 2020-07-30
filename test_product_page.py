8c9de3746927726cc0e8922a756c479c9ee4e6c8
Ryzhikov <aleksander.ryzhikov@accenture.com>
2020-07-30 19:23:06 +0300

    Empty basket tests added.

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 from selenium.common.exceptions import NoAlertPresentException # в начале файла
 import math
 import pytest
@@ -76,13 +77,13 @@ def test_message_disappeared_after_adding_product_to_basket(browser,link = "http
     solve_quiz_and_get_code(product_page)
 
     product_page.success_message_is_disappeared()
-
+@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page (browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -90,3 +91,12 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_products()
+    basket_page.empty_basket_message_present()
